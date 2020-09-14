e1de081e8e22a6bd564bfeb54dde8646014a09c9
Bianca00800 <lixandrubianca01@yahoo.ro>
2020-09-14 17:01:47 +0300

    Added EnergyCalculator class.

---
+++ Assets/Scripts/EnergyCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class EnergyCalculator 
+{
+    private double CurrentEnergy
+    {
+        get;
+        set;
+    }
+
+    private int numberOfRays;
+    private double startEnergy;
+    private float absorptionCoefficient;
+
+    public EnergyCalculator(int nrOfRays, double startE)
+    {
+        numberOfRays = nrOfRays;
+        startEnergy = startE;
+    }
+
+    private double CalculateEnergyInPoint(System.Numerics.Vector3 position, double distance)
+    {
+        /*
+         E = Ef/ Nr D_theta_phi e^-gama_d prod(1-alfa_i)
+
+        Ef - total energy emitted by the sound
+        Nr - number of rays
+        D_theta_phi - source directively; used D_theta_phi = 1
+        gama -air absorbtion
+        d - distance path traveld by the ray
+        alfa_i - absorbtion coefficient at surface i
+         */
+
+        const double airAbsorption = 0.0013;
+        double energy = startEnergy / numberOfRays * Math.Exp(-airAbsorption * distance) * (1 - absorptionCoefficient);
+
+        return energy;
+    }
+
+    private double GetDistance(System.Numerics.Vector3 start, System.Numerics.Vector3 end)
+    {
+        return Math.Sqrt(Math.Pow(start.X - end.X, 2) + Math.Pow(start.Y - end.Y, 2) + Math.Pow(start.Z - end.Z, 2));
+    }
+
+    public void CalculateEnergy(List<List<System.Numerics.Vector3>> linesPositions)
+    {
+        for (int indexRay = 0; indexRay < linesPositions.Count; ++indexRay)
+        {
+            CurrentEnergy = startEnergy;
+            double distance = 0;
+            for (int indexPoint = 1; indexPoint < linesPositions[indexRay].Count; ++indexPoint)
+            {
+                distance += GetDistance(linesPositions[indexRay][indexPoint - 1],
+                    linesPositions[indexRay][indexPoint]);
+                double energy = CalculateEnergyInPoint(linesPositions[indexRay][indexPoint], distance);
+                Debug.Log(energy + " " + indexRay + "  " + indexPoint);
+            }
+        }
+    }
+}
--- Assets/Scripts/RayGenerator.cs
+++ Assets/Scripts/RayGenerator.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RayGenerator : MonoBehaviour
 {
     public double startEnergy = 25;
-    public double absorptionCoefficient = 0.2;
-    public double minimumEnergy = 0;
-
     private int numberOfColissions = 3;
     private int maxDistance = 200;
     private readonly int numberOfRays = 3;
@@ -14,6 +10,7 @@ public class RayGenerator : MonoBehaviour
     private LineRenderer[] lines;
     private RayGeometry rayGeometryGenerator;
     private RaysDrawer rayDrawer;
+    private EnergyCalculator energyCalculator;
 
     private void Start()
     {
@@ -24,48 +21,13 @@ public class RayGenerator : MonoBehaviour
             maxDistance);
         rayGeometryGenerator.GenerateRays();
         rayDrawer = new RaysDrawer(lines, rayGeometryGenerator.LinePosistions);
-    }
-
-    private double CurrentEnergy
-    {
-        get;
-        set;
-    }
-
-    private double CalculateEnergyInPoint(Vector3 position, double distance)
-    {
-        /*
-         E = Ef/ Nr D_theta_phi e^-gama_d prod(1-alfa_i)
-
-        Ef - total energy emitted by the sound
-        Nr - number of rays
-        D_theta_phi - source directively; used D_theta_phi = 1
-        gama -air absorbtion
-        d - distance path traveld by the ray
-        alfa_i - absorbtion coefficient at surface i
-         */
-
-        double airAbsorption = 0.0013;
-        double energy = startEnergy / numberOfRays * Math.Exp(-airAbsorption * distance) * (1 - absorptionCoefficient);
-
-        return energy;
-    }
-
-    public Tuple<double, double, double> GetPolarCoordinates(Vector3 position)
-    {
-        double distanceFromOrigin = Math.Sqrt(Math.Pow(position.x, 2) +
-                                    Math.Pow(position.y, 2) +
-                                    Math.Pow(position.z, 2));
-        double theta = Math.Atan(position.y / position.x);
-        double phi = Math.Acos(position.z / distanceFromOrigin);
-
-        /*This transformation is in radians.*/
-        return Tuple.Create(distanceFromOrigin, theta, phi);
+        energyCalculator = new EnergyCalculator(numberOfRays, startEnergy);
     }
 
     private void Update()
     {
         rayGeometryGenerator.GenerateRays();
         rayDrawer.Draw();
+        energyCalculator.CalculateEnergy(rayGeometryGenerator.LinePosistions);
     }
 }
