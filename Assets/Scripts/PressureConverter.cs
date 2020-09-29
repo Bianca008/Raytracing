df6e88584ec586264cdca204e8785cf3f37e4b90
Bianca00800 <lixandrubianca01@yahoo.ro>
2020-09-29 16:26:50 +0300

    Added PressureConverter class.

---
+++ Assets/Scripts/PressureConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class PressureConverter 
+{
+    public static double ConvertIntensityToPressure(double intensity,
+        double massDensity = 1.2041,
+        double airSoundSpeed = 343.21)
+    {
+        return Math.Sqrt(intensity*massDensity*airSoundSpeed);
+    } 
+}
