5147e000e25adc291386fe2505cfd5c066b927f1
Bianca00800 <lixandrubianca01@yahoo.ro>
2020-10-05 14:01:57 +0300

    Modified ray structure.

--- Assets/Scripts/AcousticRay.cs
+++ Assets/Scripts/AcousticRay.cs
@@ -54,12 +54,10 @@ public class AcousticRay
         Source = source;
         MicrophonePosition = microphone;
         ColissionPoints = new List<Vector3>();
-        ColissionPoints.Add(Source);
         AcousticMaterials = new List<AcousticMaterial>();
         GameObject gameObject = new GameObject();
-        AcousticMaterials.Add(gameObject.AddComponent<AcousticMaterial>());
         Intensities = new List<double>();
-        Distances =new List<double>();
+        Distances = new List<double>();
     }
 
     public AcousticRay TruncateRay(int position, Vector3 microphonePos)
@@ -67,22 +65,23 @@ public class AcousticRay
         AcousticRay newRay = new AcousticRay(Source, microphonePos);
         /*index, number of elements to copy*/
         newRay.ColissionPoints = ColissionPoints.GetRange(0, position);
-        newRay.ColissionPoints.Add(microphonePos);
         /* TODO: see acoustic material for last acoustic material */
-        newRay.AcousticMaterials = AcousticMaterials.GetRange(0, position + 1);
+        newRay.AcousticMaterials = AcousticMaterials.GetRange(0, position);
 
         return newRay;
     }
 
     private float GetDistance()
     {
-        float distance = 0;
+        if (ColissionPoints.Count == 0)
+            return Vector3.Distance(Source, MicrophonePosition);
+
+        float distance = Vector3.Distance(Source,
+            ColissionPoints[0]);
 
         for (int index = 0; index < ColissionPoints.Count - 1; ++index)
         {
-            distance += (float)Math.Sqrt(Math.Pow(ColissionPoints[index].X - ColissionPoints[index + 1].X, 2) +
-                                  Math.Pow(ColissionPoints[index].Y - ColissionPoints[index + 1].Y, 2) +
-                                  Math.Pow(ColissionPoints[index].Z - ColissionPoints[index + 1].Z, 2));
+            distance += Vector3.Distance(ColissionPoints[index], ColissionPoints[index + 1]);
         }
 
         return distance;
