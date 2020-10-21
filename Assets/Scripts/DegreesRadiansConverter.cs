eac594a0135935f546279fbb3f9b438ef0624790
Bianca00800 <lixandrubianca01@yahoo.ro>
2020-10-21 15:14:56 +0300

    Added Degrees Radians converter.

---
+++ Assets/Scripts/DegreesRadiansConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+public class DegreesRadiansConverter 
+{
+    public static Vector3 TransformToPolarCoordinates(Vector3 position)
+    {
+        double distanceFromOrigin = Math.Sqrt(Math.Pow(position.X, 2) +
+            Math.Pow(position.Y, 2) + 
+            Math.Pow(position.Z, 2));
+
+        double theta = Math.Atan(position.Y / position.X);
+        double phi = Math.Acos(position.Z / distanceFromOrigin);
+
+        return new Vector3((float)distanceFromOrigin, (float)theta, (float)phi);
+    }
+
+    public static Vector3 TransformToCartesianCoordinates(double distance, double theta, double phi)
+    {
+        /*Sin and Cos are using radians.*/
+        double x = distance * Math.Sin(phi) * Math.Cos(theta);
+        double y = distance * Math.Sin(phi) * Math.Sin(theta);
+        double z = distance * Math.Cos(phi);
+
+        return new Vector3((float)x, (float)y, (float)z);
+    }
+}
---
+++ Assets/Scripts/FileReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FileReader
+{
+    public static Tuple<List<float>, List<float>> ReadFromFile(string fileName)
+    {
+        List<float> x = new List<float>();
+        List<float> y = new List<float>();
+        using (System.IO.StreamReader file = new System.IO.StreamReader(fileName))
+        {
+            while (!file.EndOfStream)
+            {
+                string text = file.ReadLine();
+                string[] bits = text.Split(' ');
+                x.Add(System.Single.Parse(bits[0]));
+                y.Add(System.Single.Parse(bits[1]));
+            }
+        }
+
+        return new Tuple<List<float>, List<float>>(x, y);
+    }
+}
