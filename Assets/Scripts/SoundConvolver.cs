92442725b2877c5b118da5ca047f196552c7477c
Bianca008 <lixandrubianca01@yahoo.ro>
2020-10-29 14:27:40 +0200

    Added SoundConvolver class.

---
+++ Assets/Scripts/SoundConvolver.cs
@@ -0,0 +1,63 @@
+﻿using NWaves.Audio;
+using NWaves.Operations;
+using NWaves.Signals;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using UnityEngine;
+
+public class SoundConvolver 
+{
+    public static void ConvolveSound(
+        AudioSource audioSource,
+        Dictionary<int, List<Complex>> frequencyReponse,
+        List<MicrophoneSphere> microphones)
+    {
+        for (int indexMicro = 0; indexMicro < microphones.Count; ++indexMicro)
+        {
+            DiscreteSignal attention;
+
+            // load
+            using (var stream = new FileStream(audioSource.clip.name + ".wav", FileMode.Open))
+            {
+                var waveFile = new WaveFile(stream);
+                attention = waveFile[Channels.Left];
+            }
+
+            Debug.Log(frequencyReponse[microphones[indexMicro].Id].Count);
+            NWaves.Transforms.RealFft value = new NWaves.Transforms.RealFft(frequencyReponse[0].Count);
+
+            List<float> re = new List<float>();
+            List<float> im = new List<float>();
+
+            foreach (Complex response in frequencyReponse[0])
+            {
+                re.Add((float)response.Real);
+                im.Add((float)response.Imaginary);
+            }
+
+            float[] outputArray = new float[re.Count];
+            value.Inverse(re.ToArray(), im.ToArray(), outputArray);
+            float maxi = outputArray.Max();
+
+            for (int index = 0; index < outputArray.Length; ++index)
+                outputArray[index] /= maxi;
+
+            for (int index = 0; index < outputArray.Length; ++index)
+                outputArray[index] *= 0.5f;
+
+            DiscreteSignal impulseRespone = new DiscreteSignal(22050, outputArray);
+            DiscreteSignal convolutionResult = Operation.Convolve(attention, impulseRespone);
+
+            // save
+            using (var stream = new FileStream("results/convolutionAttention" +
+                microphones[indexMicro].Id.ToString() +
+                ".wav", FileMode.Create))
+            {
+                var waveFile = new WaveFile(convolutionResult);
+                waveFile.SaveTo(stream);
+            }
+        }
+    }
+}
