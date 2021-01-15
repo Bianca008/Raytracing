9b649dd859c3c1b8b303a47119391ff2c00dda18
Bianca <lixandrubianca01@yahoo.ro>
2021-01-15 11:54:43 +0200

    Added scene handler class.

---
+++ Assets/Scripts/SceneHandler/SceneHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void OpenRoomA()
+    {
+        SceneManager.LoadScene("RoomAscene");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
