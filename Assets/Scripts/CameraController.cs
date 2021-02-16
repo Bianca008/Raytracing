81664e46c7ee00df9611369635fec9996d6b30e2
Bianca <lixandrubianca01@yahoo.ro>
2021-02-16 18:46:55 +0200

    Modified camera and added possibility to drag to see objects in scene. Removed Player prefab. Resetting values for camera when changing from a view to other.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,9 @@ public class CameraController : MonoBehaviour
     public Camera mainCamera;
     public Camera secondCamera;
 
+    private readonly Vector3 defaultPosMainCamera = new Vector3(1, 3.5f, -2);
+    private readonly Vector3 defaultPosSecondCamera = new Vector3(0, 5, 0);
+
     void Start()
     {
         mainCamera.enabled = true;
@@ -15,14 +18,22 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetKeyDown("o"))
         {
+            mainCamera.transform.position = defaultPosMainCamera;
             mainCamera.enabled = true;
             secondCamera.enabled = false;
         }
 
         if (Input.GetKeyDown("p"))
         {
+            secondCamera.transform.position = defaultPosSecondCamera;
             mainCamera.enabled = false;
             secondCamera.enabled = true;
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            mainCamera.transform.position = defaultPosMainCamera;
+            secondCamera.transform.position = defaultPosSecondCamera;
         }
     }
 }
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,87 +2,55 @@
 
 public class CameraFollow : MonoBehaviour
 {
-    public Vector2 clampInDegrees = new Vector2(360, 180);
-    public Vector2 sensitivity = new Vector2(2, 2);
-    public Vector2 smoothing = new Vector2(3, 3);
-    public Vector2 targetDirection;
-    public Vector2 targetCharacterDirection;
+    private float panSpeed = 10;
+    private Vector3 panLimit = new Vector3(30, 15, 30);
+    private float speed = 2f;
 
-    public Vector3 mouseOrigin = new Vector3();
-    private bool m_isRottating = false;
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    public float turnSpeed = 200.0F;
-    private Vector3 m_moveDirection = Vector3.zero;
-    private Vector2 m_mouseAbsolute;
-    private Vector2 m_smoothMouse;
-
-    // Assign this if there's a parent object controlling motion, such as a Character Controller.
-    // Yaw rotation will affect this object instead of the camera if set.
-    // Use this for initialization
     void Start()
     {
-        mouseOrigin = Input.mousePosition;
 
-        // Set target direction to the camera's initial orientation.
-        targetDirection = transform.localRotation.eulerAngles;
-
-        // Set target direction for the character body to its inital state.
-        Vector3 vec = transform.localRotation.eulerAngles;
-        targetCharacterDirection = vec;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-            m_isRottating = !m_isRottating;
+        Vector3 pos = transform.position;
 
-        if (m_isRottating == false)
-            return;
-
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
+        if (Input.GetKey("w"))
         {
-            m_moveDirection = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
-            m_moveDirection = transform.TransformDirection(m_moveDirection);
-            m_moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                m_moveDirection.y = jumpSpeed;
+            pos.z += panSpeed * Time.deltaTime;
         }
+        if (Input.GetKey("d"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("q"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("e"))
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, 1, panLimit.y);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.z, panLimit.z);
+        transform.position = pos;
 
-        m_moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(m_moveDirection * Time.deltaTime);
-
-        // Allow the script to clamp based on a desired target value.
-        var targetOrientation = Quaternion.Euler(targetDirection);
-        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
-
-        // Get raw mouse input for a cleaner reading on more sensitive mice.
-        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-
-        // Scale input against the sensitivity setting and multiply that against the smoothing value.
-        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
-
-        // Interpolate mouse movement over time to apply smoothing delta.
-        m_smoothMouse.x = Mathf.Lerp(m_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
-        m_smoothMouse.y = Mathf.Lerp(m_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
-
-        // Find the absolute mouse movement value from point zero.
-        m_mouseAbsolute += m_smoothMouse;
-
-        // Clamp and apply the local x value first, so as not to be affected by world transforms.
-        if (clampInDegrees.x < 360)
-            m_mouseAbsolute.x = Mathf.Clamp(m_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
-
-        // Then clamp and apply the global y value.
-        if (clampInDegrees.y < 360)
-            m_mouseAbsolute.y = Mathf.Clamp(m_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
-
-        GameObject.Find("Head").transform.localRotation = Quaternion.AngleAxis(-m_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
-
-        // If there's a character body that acts as a parent to the camera
-        var yRotation = Quaternion.AngleAxis(m_mouseAbsolute.x, Vector3.up);
-        transform.localRotation = yRotation * targetCharacterOrientation;
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+            float X = transform.rotation.eulerAngles.x;
+            float Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
     }
+
 }
