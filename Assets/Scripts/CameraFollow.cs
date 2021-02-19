a82a6d12e739ce04b384cb22665f805a8b6b8a42
Bianca <lixandrubianca01@yahoo.ro>
2021-02-19 18:45:40 +0200

    Solved camera problem.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,37 +13,6 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
-        Vector3 pos = transform.position;
-
-        if (Input.GetKey("w"))
-        {
-            pos.z += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("q"))
-        {
-            pos.y += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            pos.z -= panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos.x -= panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("e"))
-        {
-            pos.y -= panSpeed * Time.deltaTime;
-        }
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.y = Mathf.Clamp(pos.y, 1, panLimit.y);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.z, panLimit.z);
-        transform.position = pos;
-
         if (Input.GetMouseButton(0))
         {
             transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
@@ -51,6 +20,36 @@ public class CameraFollow : MonoBehaviour
             float Y = transform.rotation.eulerAngles.y;
             transform.rotation = Quaternion.Euler(X, Y, 0);
         }
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(transform.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(transform.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Translate(transform.up * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(-transform.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-transform.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Translate(-transform.up * panSpeed * Time.deltaTime, Space.World);
+        }
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, 1, panLimit.y);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.z, panLimit.z);
+        transform.position = pos;
     }
 
 }
